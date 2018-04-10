0662e6804f8e6feb5c9d195a389a061cd26a221e
Ale Silveri <silveridev@gmail.com>
2018-04-10 22:56:01 +0200

    server.js was created

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducers from './reducers';
+import App from './container/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(reducers);
+
+const Root = () => (
+    <Provider store={store} >
+        <App />
+    </Provider>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
